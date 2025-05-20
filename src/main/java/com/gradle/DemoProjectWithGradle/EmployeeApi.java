964e668c3b01823c65bad08e04810f30e32ee422
nikhil-biradar <nikhilbiradar878@gmail.com>
2025-05-20 16:56:26 +0530

    first commit

---
+++ src/main/java/com/gradle/DemoProjectWithGradle/EmployeeApi.java
@@ -0,0 +1,15 @@
+package com.gradle.DemoProjectWithGradle;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeApi {
+
+    @GetMapping("/{empId}")
+    public String getEmployee(String empId){
+        return "Employee Found";
+    }
+}
