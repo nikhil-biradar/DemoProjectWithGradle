dfb898eb53679aaed25c334f4794c645057c9bdd
nikhil-biradar <nikhilbiradar878@gmail.com>
2025-06-16 00:07:10 +0530

    Added new feature

---
+++ src/main/java/com/gradle/DemoProjectWithGradle/New.java
@@ -0,0 +1,7 @@
+package com.gradle.DemoProjectWithGradle;
+
+public class New {
+    public static void main(String [] args){
+        System.out.println("Hi I've made some changes to this file.");
+    }
+}
