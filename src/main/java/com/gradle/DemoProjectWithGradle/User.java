b6e41b53c74d2e2a6d0d2d3fb8a5555b0bc837b4
nikhil-biradar <nikhilbiradar878@gmail.com>
2025-06-16 12:07:03 +0530

    user entity updated

--- src/main/java/com/gradle/DemoProjectWithGradle/User.java
+++ src/main/java/com/gradle/DemoProjectWithGradle/User.java
@@ -1,7 +1,7 @@
 package com.gradle.DemoProjectWithGradle;
 
 public class User {
-    private int id;
+    private int userId;
     private String name;
     private String address;
     private double salary;
