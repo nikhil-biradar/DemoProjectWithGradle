56c1aa214f291ca825f636ca7fa18225511b1a6c
nikhil-biradar <nikhilbiradar878@gmail.com>
2025-06-16 11:55:15 +0530

    user entity added

---
+++ src/main/java/com/gradle/DemoProjectWithGradle/User.java
@@ -0,0 +1,9 @@
+package com.gradle.DemoProjectWithGradle;
+
+public class User {
+    private int id;
+    private String name;
+    private String address;
+    private double salary;
+    private int age;
+}
